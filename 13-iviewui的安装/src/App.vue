a800bd7e9dc256e0955e6610b24c556b8563b504
李丹丹email <2690905951@qq.com>
2020-08-25 15:39:43 +0800

    iviewui

---
+++ 13-iviewui的安装/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>iviewui中的插件</h1>
+        <!-- 日期选择器 -->
+        <DatePicker />
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
